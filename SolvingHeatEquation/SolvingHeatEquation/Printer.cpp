16a86619328166e7b34b90498700eb9a6b569e47
lpinbelloc <l.pinbelloc@net.estia.fr>
2021-12-01 17:55:55 +0000

    Update Printer.cpp

--- SolvingHeatEquation/SolvingHeatEquation/Printer.cpp
+++ SolvingHeatEquation/SolvingHeatEquation/Printer.cpp
@@ -9,13 +9,25 @@ Printer::Printer(std::vector<std::vector<double>> vectors) {
 void Printer::print(Parameters parameters, std::string filename, std::vector<std::vector<double>> schemesolutions) {
 	std::ofstream ofs("../OutPutFiles/" + filename + ".csv");
 	ofs << "x;Scheme;Truncation Error;Analytical Solution" << std::endl;
+	std::vector<double> errorVector;
 
 	for (int unsigned t = 0; t < schemesolutions.size(); t++) {
 		ofs << "T= " << (double)t * 0.1 << std::endl;
 		for (int x = 0; x < parameters.getSpacePoints(); x++) {
 			ofs << x * 0.05 << ";" << schemesolutions[t][x] << ";" << std::abs(schemesolutions[t][x] - analyticalSolutions[t][x]) << ";" << analyticalSolutions[t][x] << std::endl;
+			errorVector.push_back(std::abs(schemesolutions[t][x] - analyticalSolutions[t][x]));
 		}
 	}
+
+	double sumL1 = 0.;
+	double sumL2 = 0.;
+	//Calcul des normes
+	for (int unsigned i = 0; i < errorVector.size(); i++) {
+		sumL1 += errorVector[i];
+		sumL2 += pow(errorVector[i], 2);
+	}
+	ofs << "L1;" << sumL1 << std::endl;
+	ofs << "L2;" << sqrt(sumL2) << std::endl;
 }
 
 void Printer::printAnalytical(Parameters parameters) {
