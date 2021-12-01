025719870476a448761690b01c14d0e43fb77469
xabadin <80093086+xabadin@users.noreply.github.com>
2021-12-01 16:11:33 +0100

    Delete useless if

--- SolvingHeatEquation/SolvingHeatEquation/ExplicitSchemes.cpp
+++ SolvingHeatEquation/SolvingHeatEquation/ExplicitSchemes.cpp
@@ -31,16 +31,12 @@ std::vector<std::vector<double>> ExplicitSchemes::solve(Parameters parameters, d
 				v2[i] = NextTimeStep(parameters, i, DeltaT);
 			}
 		}
-
 		//to get only the 0.1's values
 		if (n % ((parameters.getVecTimePoints()[0]) / parameters.getVecOutputTimePoints().size()) == 0) {
 			schemeSolutions.push_back(v2);
 		}
-
-		if (n < meshsize_t) {
-			v0 = v1;
-			v1 = v2;
-		}
+		v0 = v1;
+		v1 = v2;
 	}
 	clock_t end = clock();
 	setComputationalTime(1000.0 * ((double)end - (double)start) / (double)CLOCKS_PER_SEC);
